dc4f68875e98b75e51f0bec12067b6cfc6c96ed6
mr.myatthu <myatthu@comquas.com>
2019-01-25 17:29:59 +0630

    Start user management

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::all();
+        $users = User::paginate();
 
         return view('admin.users.index', compact('users'));
     }
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,2 +1,55 @@
 @extends('admin.layouts.master')
 
+@section ('container')
+<div class="row">
+  <div class="col-12">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Users</h4>
+        
+        <div class="row">
+          <div class="col-12">
+            <div class="table-responsive">
+              <table id="order-listing" class="table">
+                <thead>
+                  <tr class="bg-primary text-white">
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Role</th>
+                    <th>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  @foreach($users as $index => $user)
+                  <tr>
+                    <td>{{ $index + 1}}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->phone }}</td>
+                    <td>
+                      <label class="badge badge-{{ $user->label() }}">{{ $user->roleName() }}</label>
+                    </td>
+                    <td class="text-right">
+                      <button class="btn btn-light">
+                        <i class="mdi mdi-eye text-primary"></i>View </button>
+                      <button class="btn btn-light">
+                        <i class="mdi mdi-close text-danger"></i>Remove </button>
+                    </td>
+                  </tr>
+                  @endforeach
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
+
+@section('custom-js')
+{{-- <script src="{{ asset('assets/js/shared/data-table.js') }}"></script> --}}
+@endsection
