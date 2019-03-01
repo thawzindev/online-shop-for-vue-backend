89dcf5df3be39547babb0a477f9047d9298d4208
Pymyo Swe <pymyoswe@comquas.com>
2019-03-01 09:34:38 +0630

    add home blade desigin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,113 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+@section('style')
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<style>
+        
+* {
+  margin: 0;
+  padding: 0;
+}
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+a {
+  text-decoration: none;
+}
+
+body {
+  font-weight: 600;
+  color: #343434;
+}
+
+.error_section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+  background-image: linear-gradient(-225deg, #1A1A1A, #343434);
+}
+.error_section_subtitle {
+  color: #25F193;
+  text-transform: uppercase;
+  letter-spacing: 5pt;
+  font-weight: 500;
+  font-size: 0.8rem;
+  margin-bottom: -5em;
+}
+.error_section .error_title {
+  --x-shadow: 0;
+  --y-shadow: 0;
+  --x:50%;
+  --y:50%;
+  font-size: 15rem;
+  transition: all 0.2s ease;
+  position: relative;
+  padding: 2rem;
+}
+.error_section .error_title:hover {
+  transition: all 0.2s ease;
+  text-shadow: var(--x-shadow) var(--y-shadow) 10px #1A1A1A;
+}
+.error_section .error_title p {
+  position: absolute;
+  top: 2rem;
+  left: 2rem;
+  background-image: radial-gradient(circle closest-side, rgba(255, 255, 255, 0.05), transparent);
+  background-position: var(--x) var(--y);
+  background-repeat: no-repeat;
+  text-shadow: none;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  transition: all 0.1s ease;
+}
+
+.btn {
+  padding: 0.8em 1.5em;
+  border-radius: 99999px;
+  background-image: linear-gradient(to top, #03A9F4, #00BCD4);
+  box-shadow: 0px 2px 5px 0px rgba(0, 0, 0, 0.2), inset 0px -2px 5px 0px rgba(0, 0, 0, 0.2);
+  border: none;
+  cursor: pointer;
+  text-shadow: 0px 1px #343434;
+  color: white;
+  text-transform: uppercase;
+  letter-spacing: 1.5pt;
+  font-size: 0.8rem;
+  font-weight: 700;
+  transition: ease-out 0.2s all;
+}
+.btn:hover {
+  text-shadow: 0px 1px 1px #ffffff;
+  transform: translateY(-5px);
+  box-shadow: 0px 4px 15px 2px rgba(0, 0, 0, 0.1), inset 0px -3px 7px 0px rgba(0, 0, 0, 0.2);
+  transition: ease-out 0.2s all;
+}
+
+
+</style>
+
 @endsection
+
+@section('content')
+
+    <section class="error_section">
+      <p class="error_section_subtitle">SORRY!
+The page you’re looking for was not found.</p>
+      <h1 class="error_title">
+        <p>404</p>
+        404
+      </h1>
+      <a class="btn" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        LOGOUT
+                                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+    </section>
+
+@endsection
+
+
